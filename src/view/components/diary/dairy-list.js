29b9a5355741f3161a8dd840a32d1de23d897e15
maxie030 <max.verheul@student.hu.nl>
2024-02-18 17:53:35 +0100

    Now have total eaten calories.

--- src/view/components/diary/dairy-list.js
+++ src/view/components/diary/dairy-list.js
@@ -3,29 +3,51 @@ import {repeat} from 'lit/directives/repeat.js';
 import "../product/dairy-productlist"
 import { Router } from '@vaadin/router';
 import { BASE } from '../../../app';
+import {Task} from '@lit/task';
+import { getIntakes } from '../../../service/ApiService';
+
 
 export default class DairyList extends LitElement {
     static get properties() {
         return{
             title: {type: String},
-            data: {type: Array},
-            _total: {type: Number, state: true}
+            _data: {type: Array, state: true},
+            _total: {type: Number, state: true},
+            mealtype: {type: String}
 
         }
       };
+      _dairyTask = new Task(this, {
+        task: async ([mealtype]) => {
+            var dateString = new Date(new Date().getTime() - (new Date().getTimezoneOffset() * 60000 ))
+                .toISOString()
+                .split("T")[0];
+            return await getIntakes(dateString).then(e => {
+                e.map(data => {
+                    if(data.mealType === mealtype) {   
+                        data.products.map(data => {
+                            this._data = [...this._data, data]
+                            this._total += data.nutritions.calories
+                        })
+                    }
+                })
+                this.dispatchEvent(new CustomEvent("getTotalCal", 
+                    {detail: this._total}
+                ))
+                return e;
+            })
+        },
+        args: () => [this.mealtype]
+    })
     
     constructor() {
         super();
         this.title = "default"
-        this.data = [];
+        this._data = [];
         this._total = 0;
+        this.mealtype = "";
     }
-    connectedCallback() {
-        super.connectedCallback() 
-        this.data.map(e => {
-            this._total += e.nutritions.calories
-        })
-    }
+
     static get styles() {
         return css`
         .container {
@@ -66,7 +88,11 @@ export default class DairyList extends LitElement {
                     <span>${this._total}</span>
                 </div>
                 <div class="products">
-                    <dairy-productlist .products=${this.data}></dairy-productlist>
+                    ${this._dairyTask.render({
+                        pending: () => html`<span>Loading...</span>`,
+                        complete: (e) => html`<dairy-productlist .products=${this._data}></dairy-productlist>`,
+                        error: (e) => html`<dairy-productlist></dairy-productlist>`
+                    })}
                 </div>
                 <div class="add-product" @click="${this.handleClick}">
                     <span>Add a product</span>
--- src/view/pages/dairy.js
+++ src/view/pages/dairy.js
@@ -1,58 +1,17 @@
 import { LitElement, html, css } from "lit";
 import "../components/page/page"
 import '../components/diary/dairy-list'
-import { getIntakes } from "../../service/ApiService";
-import {Task} from '@lit/task';
 
 export default class Dairy extends LitElement{
     static get properties() {
         return{
+            _totalCal: {type: Number, state: true}
         }
     }
 
-    _breakFastTask = new Task(this, {
-        task: async () => {
-            var dateString = new Date(new Date().getTime() - (new Date().getTimezoneOffset() * 60000 ))
-                .toISOString()
-                .split("T")[0];
-            return await getIntakes(dateString).then(e => {
-                return e.map(data => {
-                    if(data.mealType === "BREAKFAST") {    
-                        data.products.map(data => {
-                            this._breakfast = [...this._breakfast, data]
-                            return;
-                        })
-                    }
-                    if(data.mealType === "LUNCH") {    
-                        data.products.map(data => {
-                            this._lunch = [...this._lunch, data]
-                            return;
-                        })
-                    }
-                    if(data.mealType === "DINER") {    
-                        data.products.map(data => {
-                            this._diner = [...this._diner, data]
-                            return;
-                        })
-                    }
-                    if(data.mealType === "SNACK") {    
-                        data.products.map(data => {
-                            this._snack = [...this._snack, data]
-                            return;
-                        })
-                    }
-                })
-            })
-        },
-        args: () => []
-    })
-
     constructor() {
         super();
-        this._breakfast = [];
-        this._lunch = [];
-        this._diner = [];
-        this._snack = [];
+        this._totalCal = 0;
     }
 
     static get styles(){
@@ -93,23 +52,18 @@ export default class Dairy extends LitElement{
         <page-div>
             <div class="container">
                 <h1>Dairy</h1>
+                <h2>${this._totalCal}</h2>
                 <div class="container-dairy">
-                    ${this._breakFastTask.render({
-                        pending: () => html`<span>Loading...</span>`,
-                        complete: (e) => html`
-                            <dairy-list title="Breakfast" .data=${this._breakfast}></dairy-list>
-                            <dairy-list title="Lunch" .data=${this._lunch}></dairy-list>
-                            <dairy-list title="Diner" .data=${this._diner}></dairy-list>
-                            <dairy-list title="Snack" .data=${this._snack}></dairy-list>
-                            `,
-                        error: (e) => html`<dairy-list title="Breakfast" .data=${this._breakfast}></dairy-list>
-                            <dairy-list title="Lunch" .data=${this._lunch}></dairy-list>
-                            <dairy-list title="Diner" .data=${this._diner}></dairy-list>
-                            <dairy-list title="Snack" .data=${this._snack}></dairy-list>`
-                    })}
+                    <dairy-list title="Breakfast" mealtype="BREAKFAST" @getTotalCal="${this._setTotalCal}"></dairy-list>
+                    <dairy-list title="Lunch" mealtype="LUNCH" @getTotalCal="${this._setTotalCal}"></dairy-list>
+                    <dairy-list title="Diner" mealtype="DINER" @getTotalCal="${this._setTotalCal}"></dairy-list>
+                    <dairy-list title="Snack" mealtype="SNACK" @getTotalCal="${this._setTotalCal}"></dairy-list>
                 </div>
             </div>
         </page-div>`
+    }
+    _setTotalCal(e) {
+        this._totalCal += e.detail
     }
 
 }
